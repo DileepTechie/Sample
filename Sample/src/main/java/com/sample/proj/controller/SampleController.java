d8d00af26be569b8876738040eb302fc3f454161
dilee <dilee@Dileep>
2022-08-17 07:55:44 +0530

    First commit of Sample proj

---
+++ Sample/src/main/java/com/sample/proj/controller/SampleController.java
@@ -0,0 +1,19 @@
+package com.sample.proj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+	@GetMapping("/one")
+    public String methodOne() {
+        return "Welcome to you";
+    }
+	
+	@PostMapping("/two")
+    public String methodTwo() {
+        return "Hello";
+    }
+}
